2fe0de62bdd006bb14b8563195c0189287d2618b
Nozomu Kitamura <nozomu_kitamura@berkeley.edu>
2025-04-24 20:13:52 +0900

    手動 Info.plist 削除して Xcode 自動生成版を利用

--- SleepManagement/Models/UserSettings.swift
+++ SleepManagement/Models/UserSettings.swift
@@ -1,8 +1,19 @@
 import SwiftData
+import Foundation
 
 @Model
-struct UserSettings {
-    var id: UUID = UUID()
-    var birthYear: Int = Calendar.current.component(.year, from: Date())
-    var idealSleepDuration: TimeInterval = 8 * 3600
+final class UserSettings {
+    @Attribute(.unique) var id: UUID
+    var birthYear: Int
+    var idealSleepDuration: TimeInterval
+
+    init(
+        id: UUID = UUID(),
+        birthYear: Int = Calendar.current.component(.year, from: Date()),
+        idealSleepDuration: TimeInterval = 8 * 3600
+    ) {
+        self.id = id
+        self.birthYear = birthYear
+        self.idealSleepDuration = idealSleepDuration
+    }
 } 
